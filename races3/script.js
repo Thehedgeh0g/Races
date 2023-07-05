6ef24ac88213c0ddaada80b4b72ee17d44bcc8a3
Gregogo <87802379+Gregogo@users.noreply.github.com>
2023-07-05 15:20:52 +0300

    govno

--- races3/script.js
+++ races3/script.js
@@ -7,6 +7,9 @@ var GAME = {
 }
 
 var dial = document.getElementById('dial')
+var xdial = document.getElementById('xdial')
+var ydial = document.getElementById('ydial')
+var anglehtml = document.getElementById('angle')
 var canvas = document.getElementById('canvas');
 canvas.width = GAME.width;
 canvas.height = GAME.height;
@@ -14,8 +17,8 @@ const image = document.getElementById('img');
 var canvasContext = canvas.getContext('2d');
 var Car = new Image(); //изображение
 Car.src = '../static/sprites/abm_blue.png';//подключение изображения
-var CarPosX = 100;
-var CarPosY = 800;
+var CarPosX = -17*2;
+var CarPosY = -24*2;
 var speed = 0;
 var xspeed = 0;
 var yspeed = 0;
@@ -29,25 +32,35 @@ function drawBackground() {
 }
 
 function drawCar(image, x, y) {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     canvasContext.drawImage(image, x, y, 17*4, 24*4);
 }
 
 function UpdatePosition() {
-    CarPosY -= xspeed;
-    CarPosX -= yspeed;
+    //CarPosY -= xspeed;
+    //CarPosX -= yspeed;
+    canvasContext.rotate(-angle);
+    canvasContext.translate(xspeed, yspeed);
+    canvasContext.rotate(angle);
 }
 
 
 function drawFrame() {
     if (speed > 0) {
         speed -= 0.015625;
-        xspeed = Math.cos(angle)*speed;
-        yspeed = Math.sin(angle)*speed;
+        xspeed = Math.sin(angle)*speed;
+        yspeed = Math.cos(angle)*speed;
         dial.innerHTML = speed;
+        xdial.innerHTML = xspeed;
+        ydial.innerHTML = yspeed;
     }
     if (speed < 0) {
         speed += 0.015625;
+        xspeed = Math.sin(angle)*speed;
+        yspeed = Math.cos(angle)*speed;
         dial.innerHTML = speed;
+        xdial.innerHTML = xspeed;
+        ydial.innerHTML = yspeed;
     }
     canvasContext.clearRect(0, 0, GAME.width, GAME.height);
     drawBackground();
@@ -74,9 +87,11 @@ function onCanvasKeyDown(event) {
         if (speed < 5)
         {
             speed += 0.0625;
-            xspeed = Math.cos(angle)*speed;
-            yspeed = Math.sin(angle)*speed;
+            xspeed = Math.sin(angle)*speed;
+            yspeed = Math.cos(angle)*speed;
             dial.innerHTML = speed;
+            xdial.innerHTML = xspeed;
+            ydial.innerHTML = yspeed;
         }
     }
     if ((event.key === 's') || (event.key === 'ы')) {
@@ -84,17 +99,23 @@ function onCanvasKeyDown(event) {
         if (speed > -5)
         {
             speed -= 0.0625;
-            xspeed = Math.cos(angle)*speed;
-            yspeed = Math.sin(angle)*speed;
+            xspeed = Math.sin(angle)*speed;
+            yspeed = Math.cos(angle)*speed;
             dial.innerHTML = speed;
+            xdial.innerHTML = xspeed;
+            ydial.innerHTML = yspeed;
         }
     }
     if (((event.key === 'd') || (event.key === 'в')) && (speed != 0)) {
         angle -= 0.03;
+        anglehtml.innerHTML = angle;
+        canvasContext.rotate(0.03);
         
     }
     if (((event.key === 'a') || (event.key === 'ф')) && (speed != 0)) {
         angle += 0.03;
+        anglehtml.innerHTML = angle;
+        canvasContext.rotate(-0.03);
     }
 
 }
