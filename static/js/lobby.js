d8b68cebff9544fdcde1116749edbf6d41f6f03d
Gregogo <87802379+Gregogo@users.noreply.github.com>
2023-07-10 15:23:56 +0300

    WoW!

--- static/js/lobby.js
+++ static/js/lobby.js
@@ -15,3 +15,34 @@ function mapList() {
     }
     
 }
+
+let numberOfChildren = list.children.length;
+
+let chosenMap = 'm1'
+
+const map = [];
+
+function choosen(event) {
+    console.log(event.currentTarget.id);
+    chosenMap = event.currentTarget.id;
+    console.log(chosenMap);
+    for (let i = 1; i <= numberOfChildren; i++) {
+        if ('m'+String(i) === chosenMap) {
+            document.getElementById('m'+String(i)).style.background = "#ffdb7f";
+        } else {
+            document.getElementById('m'+String(i)).style.background = "#ffe6bf";
+        }
+    }
+}
+
+document.getElementById("button").addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
+   // var lobbyId = response.lobbyId
+    xhr.open("POST", "/api/chooseMap");
+    xhr.send(chosenMap.slice(1, 0));
+    
+    xhr.addEventListener("load", () =>{
+        console.log(xhr.responseText.substring(12, 18))
+        window.location.href = "/lobby/" + xhr.responseText.substring(12, 17)
+    })
+});
