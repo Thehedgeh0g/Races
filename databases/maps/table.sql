fe364817d6c0f309a6f597d044ad20bb6917e8a0
Thehedgeh0g <flipdoge87@gmail.com>
2023-07-10 09:43:08 +0300

    дб обновлены

--- databases/maps/table.sql
+++ databases/maps/table.sql
@@ -1,8 +1,9 @@
+use brainless_races;
 create table maps
 (
 	map_id INT NOT NULL AUTO_INCREMENT,
-	map_data text,
-    PRIMARY KEY (mapmaps_id)
+	map_data LONGTEXT,
+    PRIMARY KEY (map_id)
 ) ENGINE InnoDB,
 DEFAULT CHARACTER SET = utf8mb4,
 COLLATE utf8mb4_unicode_ci;
