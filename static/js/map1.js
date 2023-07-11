7746250d87d2b0abd7a450fd08522d48b354d693
Gregogo <87802379+Gregogo@users.noreply.github.com>
2023-07-11 15:39:52 +0300

    Car is stopping!

--- static/js/map1.js
+++ static/js/map1.js
@@ -24,14 +24,20 @@ let canvasContext = canvas.getContext('2d');
 canvasContext.imageSmoothingEnabled = false;
 let Car = new Image();
 Car.src = '../static//sprites/abm_blue.png';
-let CarPosX = -17*0.5; 
-let CarPosY = (-24*2)*0; 
+
+const carW = 17;
+const carH = 24;
+
+let CarPosX = -carW*0.5; 
+let CarPosY = (-carH*2)*0; 
 let speed = 0; 
 let xspeed = 0; 
 let yspeed = 0; 
-let angle = 0; 
+let angle = 0;
+
 let xcanvas = 0;
 let ycanvas = 0;
+
 let wasd = { 
     w: 0, 
     a: 0, 
@@ -47,6 +53,40 @@ const grassArr = [1, 2, 3, 4];
 const roadArr = [11, 10, 9, 8, 7, 6];
 const borderArr = [12];
 
+function drawFrame() {
+    onCanvasKey();
+    UpdatePosition();
+
+    drawCar(Car, CarPosX, CarPosY); 
+    framesCountHandler(); 
+    requestAnimationFrame(drawFrame);
+}
+
+function UpdatePosition() {
+    bFlag = false;
+    updateReduce();
+    reduceSpeed();
+
+    canvasContext.rotate(angle); 
+    canvasContext.translate(xspeed, yspeed); 
+    canvasContext.rotate(-angle);
+
+    y = window.getComputedStyle(move).top;
+    x = window.getComputedStyle(move).left;  
+    y = y.slice(0, -2);
+    x = x.slice(0, -2);
+    y = Number(y);
+    x = Number(x);
+
+    move.style.top = String(- yspeed + y) + 'px';
+    move.style.left =  String(- xspeed + x) + 'px';
+
+    displayDots();
+
+    xcanvas += xspeed;
+    ycanvas += yspeed;
+
+} 
 
 function initEventsListeners() { 
     window.addEventListener('keydown', onCanvasKeyDown); 
@@ -60,23 +100,13 @@ function framesCountHandler() {
     ++GAME.framesCnt; 
 } 
 
-function drawFrame() { 
-    canvasContext.clearRect(0, 0, GAME.width, GAME.height); 
-    UpdatePosition(); 
-    initEventsListeners(); 
-    drawCar(Car, CarPosX, CarPosY); 
-    framesCountHandler(); 
-    requestAnimationFrame(drawFrame);
-    onCanvasKey();
-}
-
 function drawCar(image, x, y) { 
     canvasContext.rotate(angle);
     canvasContext.translate(-xcanvas, -ycanvas);
     canvasContext.clearRect(0, 0, canvas.width, canvas.height); 
     canvasContext.translate(xcanvas, ycanvas);
     canvasContext.rotate(-angle);
-    canvasContext.drawImage(image, x, y, 17, 24);
+    canvasContext.drawImage(image, x, y, carW, carH);
 } 
 
 function divme(a, b){
@@ -86,7 +116,11 @@ function divme(a, b){
 function reduceSpeed() {
     if (speed > 0) {
         if (speed-accel <= mspeed) {
-            speed -= resist; 
+            if ((speed - resist) > 0){
+                speed -= resist; 
+            } else {
+                speed = 0;
+            }
             xspeed = Math.sin(angle)*speed; 
             yspeed = Math.cos(angle)*speed; 
         } else {
@@ -102,9 +136,11 @@ function reduceSpeed() {
     } 
     if (speed < 0) { 
         if (speed+accel >= -mspeed) {
-            speed += resist; 
-            xspeed = Math.sin(angle)*speed; 
-            yspeed = Math.cos(angle)*speed; 
+            if ((speed + resist) < 0){
+                speed += resist; 
+            } else {
+                speed = 0;
+            }
         } else {
             if ((speed + ga) < 0){
                 speed += ga; 
@@ -149,31 +185,6 @@ function updateReduce() {
         }
     }
 }
-function UpdatePosition() { 
-    canvasContext.rotate(angle); 
-    canvasContext.translate(xspeed, yspeed); 
-    canvasContext.rotate(-angle);
-
-    y = window.getComputedStyle(move).top;
-    x = window.getComputedStyle(move).left;  
-    y = y.slice(0, -2);
-    x = x.slice(0, -2);
-    y = Number(y);
-    x = Number(x);
-
-    bFlag = false;
-    updateReduce();
-    reduceSpeed();
-
-    move.style.top = String(- yspeed + y) + 'px';
-    move.style.left =  String(- xspeed + x) + 'px';
-
-    displayDots();
-
-    xcanvas += xspeed;
-    ycanvas += yspeed;
-
-} 
 
 function onCanvasKey() { 
     if (wasd.w == 1) { 
@@ -324,10 +335,10 @@ function scrollToCenter() {
     x = Number(x);
 
     O=[1440-x, 1440-y];
-    D=[O[0]+Math.cos(angle)*8.5, O[1]-Math.sin(angle)*8.5];
-    A=[O[0]-Math.cos(angle)*8.5, O[1]+Math.sin(angle)*8.5];
-    C=[D[0]+Math.sin(angle)*24, D[1]+Math.cos(angle)*24];
-    B=[A[0]+Math.sin(angle)*24, A[1]+Math.cos(angle)*24];
+    D=[O[0]+Math.cos(angle)*(carW/2), O[1]-Math.sin(angle)*(carW/2)];
+    A=[O[0]-Math.cos(angle)*(carW/2), O[1]+Math.sin(angle)*(carW/2)];
+    C=[D[0]+Math.sin(angle)*carH, D[1]+Math.cos(angle)*carH];
+    B=[A[0]+Math.sin(angle)*carH, A[1]+Math.cos(angle)*carH];
     console.log(A, B, C, D);
     r1.style.top = String(A[1]) + 'px';
     r1.style.left = String(A[0]) + 'px';
@@ -348,8 +359,6 @@ function getTiles() {
         tiles = mapping.split(' ');
         console.log(tiles);
         isLoaded = true;
-        //for (let y = 0,)
-    
     })
     xhr.send();
 }
@@ -361,6 +370,7 @@ function prepareCanvas() {
 }
 
 getTiles();
+initEventsListeners(); 
 prepareCanvas();
 scrollToCenter();
 drawFrame();
