8b4367be0b92fbc1742e64f659bc015fa6c5d1bf
ignatatylov <124980883+ignatatylov@users.noreply.github.com>
2023-07-12 16:32:25 +0300

    add upgrade

--- static/js/garage.js
+++ static/js/garage.js
@@ -15,9 +15,28 @@ const YellowCarField = document.getElementById("colorYellowCar");
 const engineArrowLeft = document.getElementById("engineArrowLeft");
 const engineArrowRight = document.getElementById("engineArrowRight");
 
+const transmissionArrowLeft = document.getElementById("transmissionArrowLeft");
+const transmissionArrowRight = document.getElementById("transmissionArrowRight");
+
+const breaksArrowLeft = document.getElementById("breaksArrowLeft");
+const breaksArrowRight = document.getElementById("breaksArrowRight");
+
+const suspensionArrowLeft = document.getElementById("suspensionArrowLeft");
+const suspensionArrowRight = document.getElementById("suspensionArrowRight");
+
+
+const tuningPurchase = document.getElementById("tuningPurchase");
+
 var engineCnt = 1;
 document.getElementById("engineCnt").innerHTML = engineCnt;
+var transmissionCnt = 1;
+document.getElementById("transmissionCnt").innerHTML = transmissionCnt;
+var breaksCnt = 1;
+document.getElementById("breaksCnt").innerHTML = breaksCnt;
+var  suspensionCnt = 1;
+document.getElementById("suspensionCnt").innerHTML = suspensionCnt;
 
+var futureEngineCnt = 1;
 
 
 ShopField.addEventListener("click", showShop);
@@ -36,6 +55,17 @@ YellowCarField.addEventListener("click", replaceCar);
 
 engineArrowLeft.addEventListener("click", engineDec);
 engineArrowRight.addEventListener("click", engineInc);
+
+transmissionArrowLeft.addEventListener("click", transmissionDec);
+transmissionArrowRight.addEventListener("click", transmissionInc);
+
+breaksArrowLeft.addEventListener("click", breaksDec);
+breaksArrowRight.addEventListener("click", breaksInc);
+
+suspensionArrowLeft.addEventListener("click", suspensionDec);
+suspensionArrowRight.addEventListener("click", suspensionInc);
+
+tuningPurchase.addEventListener("click", ShowPurchase);
 
 
 // var xhr = new XMLHttpRequest();
@@ -93,7 +123,7 @@ function replaceCar() {
 function engineDec() {
   if (engineCnt > 1){
     document.getElementById("engineCnt").innerHTML = String(engineCnt - 1);
-    document.querySelector(".MaxSpeedGraph").style.width =  String(engineCnt - 1) + "vw";
+    document.querySelector(".FutureMaxSpeedGraph").style.width =  String(engineCnt - 1) + "vw";
     engineCnt -= 1;
   }
 }
@@ -101,7 +131,61 @@ function engineDec() {
 function engineInc() {
   if (engineCnt < 10){
     document.getElementById("engineCnt").innerHTML = String(engineCnt + 1);
-    document.querySelector(".MaxSpeedGraph").style.width =  String(engineCnt + 1) + "vw";
+    document.querySelector(".FutureMaxSpeedGraph").style.width =  String(engineCnt + 1) + "vw";
     engineCnt += 1;
   }
+}
+
+function transmissionDec() {
+  if (transmissionCnt > 1){
+    document.getElementById("transmissionCnt").innerHTML = String(transmissionCnt - 1);
+    document.querySelector(".FutureAccelerationGraph").style.width =  String(transmissionCnt - 1) + "vw";
+    transmissionCnt -= 1;
+  }
+}
+
+function transmissionInc() {
+  if (transmissionCnt < 10){
+    document.getElementById("transmissionCnt").innerHTML = String(transmissionCnt + 1);
+    document.querySelector(".FutureAccelerationGraph").style.width =  String(transmissionCnt + 1) + "vw";
+    transmissionCnt += 1;
+  }
+}
+
+function breaksDec() {
+  if (breaksCnt > 1){
+    document.getElementById("breaksCnt").innerHTML = String(breaksCnt - 1);
+    document.querySelector(".FutureManeuverabilityGraph").style.width =  String((breaksCnt - 1 + suspensionCnt)/2) + "vw";
+    breaksCnt -= 1;
+  }
+}
+
+function breaksInc() {
+  if (breaksCnt < 10){
+    document.getElementById("breaksCnt").innerHTML = String(breaksCnt + 1);
+    document.querySelector(".FutureManeuverabilityGraph").style.width =  String((breaksCnt + 1 + suspensionCnt)/2) + "vw";
+    breaksCnt += 1;
+  }
+}
+
+function suspensionDec() {
+  if (suspensionCnt > 1){
+    document.getElementById("suspensionCnt").innerHTML = String(suspensionCnt - 1);
+    document.querySelector(".FutureManeuverabilityGraph").style.width =  String((suspensionCnt - 1 + breaksCnt)/2) + "vw";
+    suspensionCnt -= 1;
+  }
+}
+
+function suspensionInc() {
+  if (suspensionCnt < 10){
+    document.getElementById("suspensionCnt").innerHTML = String(suspensionCnt + 1);
+    document.querySelector(".FutureManeuverabilityGraph").style.width =  String((suspensionCnt + 1 + breaksCnt)/2) + "vw";
+    suspensionCnt += 1;
+  }
+}
+
+function ShowPurchase() {
+  document.querySelector(".MaxSpeedGraph").style.width =  String(engineCnt) + "vw";
+  document.querySelector(".AccelerationGraph").style.width =  String(transmissionCnt) + "vw";
+  document.querySelector(".ManeuverabilityGraph").style.width =  String((breaksCnt + suspensionCnt)/2) + "vw";
 }
