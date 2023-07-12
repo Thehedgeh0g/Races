7bed29decb259f1b27646c572f37ec41bfa6c963
Gregogo <87802379+Gregogo@users.noreply.github.com>
2023-07-12 13:55:51 +0300

    ed

--- static/js/lobbySocket.js
+++ static/js/lobbySocket.js
@@ -1,3 +1,15 @@
+const avatar1 = document.getElementById("avatar1");
+const nickName1 = document.getElementById("nickName1");
+const lvl1 = document.getElementById("lvl1");
+const avatar2 = document.getElementById("avatar2");
+const nickName2 = document.getElementById("nickName2");
+const lvl2 = document.getElementById("lvl2");
+const avatar3 = document.getElementById("avatar3");
+const nickName3 = document.getElementById("nickName3");
+const lvl3 = document.getElementById("lvl3");
+const avatar4 = document.getElementById("avatar4");
+const nickName4 = document.getElementById("nickName4");
+const lvl4 = document.getElementById("lvl4");
 
 var socket = new WebSocket("ws:/localhost:3000/ws");
 
