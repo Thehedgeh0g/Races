0a4fc6f174183a69e9671ec4e4ad5f444a17fab5
GluXoy <124980658+GluXoy@users.noreply.github.com>
2023-07-13 15:52:16 +0300

    Add files via upload

--- static/js/account.js
+++ static/js/account.js
@@ -1,6 +1,7 @@
 const friendsListToggler = document.getElementById("FriendsListToggler");
 const friendsData = document.querySelector(".friends-data");
 const friendsListTogglerClassList = document.querySelector(".friends-list__toggler");
+const searchFriendButton = document.getElementById("SearchFriendButton");
 
 friendsListToggler.addEventListener("click", function() {
   if (friendsData.style.display === "none" || friendsData.style.display === "") {
@@ -14,3 +15,43 @@ friendsListToggler.addEventListener("click", function() {
   }
 });
 
+
+// добавление друга
+
+const addFriends = document.getElementById("AddFriend");
+const closeButton = document.getElementById("CloseButton");
+
+addFriends.addEventListener("click", showAddFriendBox);
+
+function showAddFriendBox() {
+  addFriendClick();
+  darkenScreen();
+}
+
+function addFriendClick() {
+  document.querySelector(".add-friend-box").style.visibility = "visible";
+}
+
+let overlay = document.createElement("div");
+
+function darkenScreen() {
+  overlay.classList.add("overlay");
+  document.body.appendChild(overlay);
+}
+
+closeButton.addEventListener("click", function() {
+  document.body.removeChild(overlay);
+  document.querySelector(".add-friend-box").style.visibility = "hidden";
+})
+
+
+function changeBorderColor() { 
+  searchFriendButton.style.border = "0.3vh solid #eb6126";
+}
+
+function returnBorderColor() {
+  searchFriendButton.style.border = "0.3vh solid #a88312";
+}
+
+searchFriendButton.addEventListener("mouseover", changeBorderColor);
+searchFriendButton.addEventListener("mouseout", returnBorderColor);
